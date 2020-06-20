71d25e94186cfb4406bcf6fc97faa6b1dde8a288
obulaworld <obulaworldofexcellence@gmail.com>
2020-06-20 21:50:33 +0100

    Solution

--- src/App.js
+++ src/App.js
@@ -5,23 +5,25 @@ import Table from './components/Table';
 class App extends Component {
   constructor() {
       super()
-    this.state = {
-      parameterState: ""
-    }
+      this.state = {
+        parameterState: ""
+      }
+    this.sortByParameter = this.sortByParameter.bind(this);
   }
 
   sortByParameter(parameter) {
-    // set state of 'parameterState' here
+    this.setState({ parameterState: parameter })
   }
 
   render() {
+    const { parameterState } = this.state;
     return (
       <div className='container-fluid'>
         <center>
           <h1>Birthday Records</h1>
         </center>
-        <Radio />
-        <Table />
+        <Radio sortBy={this.sortByParameter} />
+        <Table sortParameter={parameterState} />
       </div>
 );
 
--- src/components/Radio.js
+++ src/components/Radio.js
@@ -2,14 +2,18 @@ import React, {Component} from 'react';
 import {PropTypes} from 'prop-types';
 
 class Radio extends Component {
-	constructor() {
-        super()
-		this.state = {
-			radioClick: "name"
-		};
+	constructor(props) {
+        super(props)
+        this.state = {
+          radioClick: "name",
+          selectedOption: ''
+        };
 	}
 
 	onChange(type) {
+    this.setState({ selectedOption: type })
+    const { sortBy } = this.props;
+    sortBy(type);
 		// on radio state change handler
 	}
 
@@ -17,11 +21,11 @@ class Radio extends Component {
 		return (
   <div className='radioButtons'>
     <div className='left'>
-      <input type='radio' />
+      <input id='sortName' name="sort" type='radio' onChange={() => { this.onChange('name') }} />
       <label>&nbsp;&nbsp;Sort by name</label>
     </div>
     <div className='right'>
-      <input type='radio' />
+      <input id="sortAge" name="sort" type='radio' onChange={() => { this.onChange('age') }} />
       <label>&nbsp;&nbsp;Sort by age</label>
     </div>
   </div>
@@ -30,9 +34,9 @@ class Radio extends Component {
 	}
 }
 
-// Uncomment the snippet below
-// Radio.propTypes = {
-// 	sortBy: PropTypes.func
-// }
+
+Radio.propTypes = {
+	sortBy: PropTypes.func
+}
 
 export default Radio;
